4eb75f76cad6b8f3fad5b67171fd32b0bfa92c88
VOsipenkov <jumpViaOcean@gmail.com>
2023-06-27 19:38:10 +0300

    Nuclearbot-4-create_parsing_agent_radon

---
+++ src/main/groovy/nuclear/bot/parsing/agent/radon/service/PageParser.groovy
@@ -0,0 +1,38 @@
+package nuclear.bot.parsing.agent.radon.service
+
+import lombok.extern.slf4j.Slf4j
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.springframework.stereotype.Service
+import us.codecraft.xsoup.Xsoup
+
+import java.util.logging.Logger
+
+@Slf4j
+@Service
+class PageParser {
+    Logger logger = Logger.getLogger(getClass().name)
+
+    List<String> getContent(String url, String xPath) {
+        def document = Jsoup.connect(url).get()
+        def xsoup = Xsoup.compile(xPath).evaluate(document).get()
+        def textBlock = Arrays.stream(xsoup.split("\n\t\t"))
+                .filter(value -> value.startsWith "var iconText")
+                .findFirst()
+                .map(v -> v.replaceAll"var iconText = \\[", "")
+                .map(v -> v.replaceAll"\\\\'", "")
+                .map(v -> v.replaceAll"',", "")
+                .map(v -> v.replaceAll"'", "")
+                .map(v -> v.replaceAll"];", "")
+        if (textBlock.isPresent()) {
+            def values = Arrays
+                    .stream(textBlock.get().split("<div.class=placemarkName>"))
+                    .map(v -> v.replaceAll "</div> ", "")
+                    .filter(v -> v.trim().length() != 0)
+                    .toList()
+            logger.info "Parsed values ${values.size()}"
+            return values
+        }
+       return  Collections.EMPTY_LIST
+    }
+}
