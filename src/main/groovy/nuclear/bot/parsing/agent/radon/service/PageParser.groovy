070c79c1b5d2d1be1788130d2c737682be033134
VOsipenkov <jumpViaOcean@gmail.com>
2023-06-27 20:51:55 +0300

    Nuclearbot-4-create_parsing_agent_radon

--- src/main/groovy/nuclear/bot/parsing/agent/radon/service/PageParser.groovy
+++ src/main/groovy/nuclear/bot/parsing/agent/radon/service/PageParser.groovy
@@ -1,6 +1,8 @@
 package nuclear.bot.parsing.agent.radon.service
 
+import nuclear.bot.parsing.agent.radon.core.dto.AgentMessage
 import org.jsoup.Jsoup
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import us.codecraft.xsoup.Xsoup
 
@@ -9,18 +11,24 @@ import java.util.logging.Logger
 @Service
 class PageParser {
     Logger logger = Logger.getLogger(getClass().name)
+    private final String2AgentMessageMapper string2AgentMessageMapper;
 
-    List<String> getContent(String url, String xPath) {
+    @Autowired
+    PageParser(String2AgentMessageMapper string2AgentMessageMapper) {
+        this.string2AgentMessageMapper = string2AgentMessageMapper
+    }
+
+    List<AgentMessage> getContent(String url, String xPath) {
         def document = Jsoup.connect(url).get()
         def xsoup = Xsoup.compile(xPath).evaluate(document).get()
         def textBlock = Arrays.stream(xsoup.split("\n\t\t"))
                 .filter(value -> value.startsWith "var iconText")
                 .findFirst()
-                .map(v -> v.replaceAll"var iconText = \\[", "")
-                .map(v -> v.replaceAll"\\\\'", "")
-                .map(v -> v.replaceAll"',", "")
-                .map(v -> v.replaceAll"'", "")
-                .map(v -> v.replaceAll"];", "")
+                .map(v -> v.replaceAll "var iconText = \\[", "")
+                .map(v -> v.replaceAll "\\\\'", "")
+                .map(v -> v.replaceAll "',", "")
+                .map(v -> v.replaceAll "'", "")
+                .map(v -> v.replaceAll "];", "")
         if (textBlock.isPresent()) {
             def values = Arrays
                     .stream(textBlock.get().split("<div.class=placemarkName>"))
@@ -28,8 +36,11 @@ class PageParser {
                     .filter(v -> v.trim().length() != 0)
                     .toList()
             logger.info "Parsed values ${values.size()}"
-            return values
+            return values.stream()
+                    .map(v -> string2AgentMessageMapper.map v)
+                    .toList() as List<AgentMessage>
         }
-       return  Collections.EMPTY_LIST
+        logger.info("There is empty parsed result")
+        return Collections.EMPTY_LIST
     }
 }
--- src/main/groovy/nuclear/bot/parsing/agent/radon/service/ParsingService.groovy
+++ src/main/groovy/nuclear/bot/parsing/agent/radon/service/ParsingService.groovy
@@ -1,8 +1,10 @@
 package nuclear.bot.parsing.agent.radon.service
 
 import lombok.RequiredArgsConstructor
+import nuclear.bot.parsing.agent.radon.core.dto.AgentMessage
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
@@ -17,15 +19,21 @@ class ParsingService {
     private String xPath
     Logger logger = Logger.getLogger(getClass().name)
     private final PageParser pageParser
+    private final KafkaTemplate<String, AgentMessage> kafkaTemplate
 
     @Autowired
-    ParsingService(PageParser pageParser) {
+    ParsingService(PageParser pageParser, KafkaTemplate<String, AgentMessage> kafkaTemplate) {
         this.pageParser = pageParser
+        this.kafkaTemplate = kafkaTemplate
     }
 
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 10_000)
     void parse() {
         def values = pageParser.getContent(url, xPath)
         logger.info "Parsed values ${values}"
+    }
+
+    void sendMessage(List<String> values) {
+
     }
 }
